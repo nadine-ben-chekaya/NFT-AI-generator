0ea6b8c66b4e1492b246744963c9c03f4c0f2519
nadine-ben-chekaya <nadinbenchekaya@gmail.com>
2024-05-03 07:54:21 +0200

    privacy commit

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -2,18 +2,19 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition-ethers");
 require("dotenv").config();
 
-const { PRIVATE_KEY_ACCOUNT1, ALCHEMY_API_KEY } = process.env;
+const { PRIVATE_KEY_ACCOUNT1, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } =
+  process.env;
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/FQLza3Pw812rsFJyGWZvPsJXGzRvnWNv`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY_ACCOUNT1],
     },
   },
 
   etherscan: {
-    apiKey: "1U4UF6N65R9EZMBUFW8RFAT4ZYGQEVIHXA",
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
